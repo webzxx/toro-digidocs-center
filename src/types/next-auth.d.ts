2936e13c92bd265a4b85e74f7e8c5802554c9e52
aaron <aaron_palpallatoc@dlsu.edu.ph>
2024-07-22 03:23:53 +0800

    Update DashboardSideBar component to display user-specific links

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,19 +1,19 @@
-import { UserRole } from 'prisma/client'
-import NextAuth from "next-auth"
-import type { User } from 'next-auth'
+import { UserRole } from "prisma/client";
+import NextAuth from "next-auth";
+import type { User } from "next-auth";
 
 declare module "next-auth" {
   interface User {
-    username: string
+    username: string;
   }
   interface Session {
     user: User & {
-      username: string
-      id: UserId 
-      
-    }
+      username: string;
+      id: UserId;
+      role: UserRole;
+    };
     token: {
-      username: string
-    }
+      username: string;
+    };
   }
 }
