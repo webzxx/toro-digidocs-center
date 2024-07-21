a3ed8e4d0511bb87b3a534fa6b93bbfcbb0f49e9
aaron <aaron_palpallatoc@dlsu.edu.ph>
2024-07-22 03:00:36 +0800

    Add CRUD for user in dashboard

--- src/app/dashboard/users/page.tsx
+++ src/app/dashboard/users/page.tsx
@@ -1,19 +1,44 @@
-import { Button } from '@/components/ui/button'
-import React from 'react'
+"use client";
+
+import { User } from '@prisma/client'
+import React, { useState, useEffect, useCallback } from 'react'
+import { fetchUsers } from './action'
+import UserTable from '@/components/UserTable';
 
 export default function UsersPage() {
+  const [users, setUsers] = useState<User[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  const loadUsers = useCallback(() => {
+    setLoading(true);
+    fetchUsers().then((fetchedUsers) => {
+      setUsers(fetchedUsers);
+      setLoading(false);
+    });
+  }, []);
+
+  useEffect(() => {
+    loadUsers();
+  }, [loadUsers]);
+
   return (
     <main className="flex flex-col gap-2 lg:gap-2 min-h-[90vh] w-full">
-      <div className="flex flex-1 items-center justify-center rounded-lg border border-dashed shadow-sm">
-        <div className="flex flex-col items-center text-center">
-          <h3 className="text-2xl font-bold tracking-tight">
-            You have no projects
-          </h3>
-          <p className="text-sm text-muted-foreground mb-3">
-            Projects will show when you start using RankBoost AI
-          </p>
-          <Button>Create Project</Button>
+      {loading ? (
+        <div>Loading...</div>
+      ) : users && users.length > 0 ? (
+        <UserTable users={users} onReload={loadUsers} />
+      ) : (
+        <div className="flex flex-1 items-center justify-center rounded-lg border border-dashed shadow-sm">
+          <div className="flex flex-col items-center text-center">
+            <h3 className="text-2xl font-bold tracking-tight">
+              You have no users
+            </h3>
+            <p className="text-sm text-muted-foreground mb-3">
+              Users will be shown here including their roles.
+            </p>
+          </div>
         </div>
-      </div>
-    </main>)
+      )}
+    </main>
+  );
 }
---
+++ src/components/UserTable.tsx
@@ -0,0 +1,116 @@
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardHeader,
+    CardTitle,
+  } from "@/components/ui/card";
+  import {
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+  } from "@/components/ui/table";
+  import { User, UserRole } from "@prisma/client";
+  import {
+    Tooltip,
+    TooltipContent,
+    TooltipProvider,
+    TooltipTrigger,
+  } from "@radix-ui/react-tooltip";
+  import UserActions from "./UserActions";
+  
+  interface UserTableProps {
+    users?: User[];
+    onReload: () => void;
+  }
+  
+  export default function UserTable({ users, onReload }: UserTableProps) {
+    return (
+      <Card>
+        <CardHeader className="px-7">
+          <CardTitle>Users</CardTitle>
+          <CardDescription>List of Barangay Bahay Taro users.</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead>Username</TableHead>
+                <TableHead className="hidden md:table-cell">Email</TableHead>
+                <TableHead className="hidden md:table-cell">Role</TableHead>
+                <TableHead className="hidden sm:table-cell">Password</TableHead>
+                <TableHead className="hidden sm:table-cell">Created at</TableHead>
+                <TableHead className="hidden sm:table-cell">Updated at</TableHead>
+                <TableHead className="hidden sm:table-cell">Actions</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {users && users.length > 0 && (
+                users.map((user) => (
+                  <TableRow key={user.id}>
+                    <TableCell>
+                      <div className="font-medium">{user.username}</div>
+                    </TableCell>
+                    <TableCell className="hidden sm:table-cell">
+                      {user.email.length > 20 ? (
+                        <>
+                          {/* Shorten super long emails (20 characters) */}
+                          <TooltipProvider>
+                            <Tooltip>
+                              <TooltipTrigger>
+                                {user.email.slice(0, 20)}...
+                              </TooltipTrigger>
+  
+                              {/* Tooltip wraps because email is long */}
+                              <TooltipContent className="w-60 whitespace-normal text-wrap break-words rounded-md bg-background p-2 shadow-md">
+                                <div>{user.email}</div>
+                              </TooltipContent>
+                            </Tooltip>
+                          </TooltipProvider>
+                        </>
+                      ) : (
+                        user.email
+                      )}
+                    </TableCell>
+                    <TableCell className="hidden sm:table-cell">
+                      {user.role}
+                    </TableCell>
+                    <TableCell className="hidden sm:table-cell">
+                       {/* Tooltip provides full password, shows only first 5 */}
+                       <TooltipProvider>
+                        <Tooltip>
+                          <TooltipTrigger>
+                            {user.password.slice(0, 5)}...
+                          </TooltipTrigger>
+  
+                          {/* Tooltip wraps because password is long */}
+                          <TooltipContent className="w-60 whitespace-normal text-wrap break-words rounded-md bg-background p-2 shadow-md">
+                            <div>{user.password}</div>
+                          </TooltipContent>
+                        </Tooltip>
+                      </TooltipProvider>
+                    </TableCell>
+                    <TableCell className="hidden sm:table-cell">
+                      {user.createdAt.toString()}
+                    </TableCell>
+                    <TableCell className="hidden sm:table-cell">
+                      {user.updateUt.toString()}
+                    </TableCell>
+                    <TableCell className="hidden sm:table-cell">
+                      {user.role !== UserRole.ADMIN && (
+                        <UserActions userId={user.id.toString()} username={user.username} email={user.email} onReload={onReload} />
+                      )}
+                    </TableCell>
+                  </TableRow>
+                ))
+              )}
+            </TableBody>
+          </Table>
+        </CardContent>
+      </Card>
+    );
+  }
+  
