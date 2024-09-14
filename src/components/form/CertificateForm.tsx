0861e8bfcb21d6af4aec9e9581696272623a438e
aaron <aaron_palpallatoc@dlsu.edu.ph>
2024-09-14 15:40:21 +0800

    Add simple stepper demo

--- src/components/form/CertificateForm.tsx
+++ src/components/form/CertificateForm.tsx
@@ -2,7 +2,7 @@
 
 import { useForm } from "react-hook-form";
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "../ui/form";
-import { z } from "zod"
+
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Input } from "../ui/input";
 import { Button } from "../ui/button";
@@ -10,6 +10,13 @@ import { toast } from "../ui/use-toast";
 import { DatePicker } from "../DatePicker"; // Ensure this is correctly imported
 import { CertificateSchema, CertificateInput } from "@/types/types";
 import { createCertificate } from "@/app/api/certificate/actions";
+
+import {
+  Step,
+  StepItem,
+  Stepper,
+  useStepper,
+} from "@/components/ui/stepper"
 
 type FormFieldKey =
   | "precinct"
@@ -20,188 +27,229 @@ type FormFieldKey =
   | "birthdate"
   | "contact"
 
+const steps = [
+  { label: "Personal" },
+  { label: "Address" },
+  { label: "Important Information" },
+  { label: "Proof of Identity" },
+] satisfies StepItem[]
+
+
+function StepperDemo() {
+  return (
+    <div className="flex w-full flex-col gap-4">
+      <Stepper initialStep={0} steps={steps}>
+        {steps.map((stepProps, index) => {
+          return (
+            <Step key={stepProps.label} {...stepProps}>
+              <div className="h-40 flex items-center justify-center my-2 border bg-secondary text-primary rounded-md">
+                <h1 className="text-xl">Step {index + 1}</h1>
+              </div>
+            </Step>
+          )
+        })}
+        <Footer />
+      </Stepper>
+    </div>
+  )
+}
+
+const Footer = () => {
+  const {
+    nextStep,
+    prevStep,
+    resetSteps,
+    hasCompletedAllSteps,
+    isLastStep,
+    isOptionalStep,
+    isDisabledStep,
+  } = useStepper()
+  return (
+    <>
+      {hasCompletedAllSteps && (
+        <div className="h-40 flex items-center justify-center my-2 border bg-secondary text-primary rounded-md">
+          <h1 className="text-xl">All steps completed! 🎉</h1>
+        </div>
+      )}
+      <div className="w-full flex justify-end gap-2">
+        {hasCompletedAllSteps ? (
+          <Button size="sm" onClick={resetSteps}>
+            Reset
+          </Button>
+        ) : (
+          <>
+            <Button
+              disabled={isDisabledStep}
+              onClick={prevStep}
+              size="sm"
+              variant="secondary"
+            >
+              Prev
+            </Button>
+            <Button size="sm" onClick={nextStep}>
+              {isLastStep ? "Finish" : isOptionalStep ? "Skip" : "Next"}
+            </Button>
+          </>
+        )}
+      </div>
+    </>
+  )
+}
+ 
 
 const CertificateForm = () => {
-    const form = useForm<CertificateInput>({
-        resolver: zodResolver(CertificateSchema),
-        defaultValues: {
-          precinct: '',
-          firstname: '',
-          middlename: '',
-          lastname: '',
-          email: '',
-          birthdate: '',
-          contact: ''
-      },
-      });
+  const form = useForm<typeof CertificateInput>({
+    resolver: zodResolver(CertificateSchema),
+    defaultValues: {
+      precinct: '',
+      firstname: '',
+      middlename: '',
+      lastname: '',
+      email: '',
+      birthdate: '',
+      contact: ''
+    },
+  });
 
-      const onSubmit = async (values: CertificateInput) => {
-        console.log("Form values on submit:", values); // Add this line to log values
+  const onSubmit = async (values: typeof CertificateInput) => {
+    console.log("Form values on submit:", values); // Add this line to log values
 
-        createCertificate(values).then((res)=>{
-          if (res?.fieldError) {
-             // fieldError: { email: "Invalid email "}
-            Object.entries(res.fieldError).forEach(([field, message]) => {
-              form.setError(field as FormFieldKey, {
-                type: "manual",
-                message,
-              });
-            });
-          }
+    createCertificate(values).then((res)=>{
+      if (res?.fieldError) {
+          // fieldError: { email: "Invalid email "}
+        Object.entries(res.fieldError).forEach(([field, message]) => {
+          form.setError(field as FormFieldKey, {
+            type: "manual",
+            message,
+          });
+        });
+      }
 
-          if (res?.serverError) {
-            toast({
-                title: "Error",
-                description: res.serverError || "Oops! Something went wrong!",
-                variant: "destructive"
-              });
-          }
-          
-          if (res?.success){
-            toast({
-              title: "Success",
-              description: "Certificate has been created successfully!",
-              variant: "default"
-            });
-          }
-        })
-        // try {
-        //   const response = await fetch('/api/certificate', {
-        //     method: 'POST',
-        //     headers: {
-        //       'Content-Type': 'application/json'
-        //     },
-        //     body: JSON.stringify(values)
-        //   });
+      if (res?.serverError) {
+        toast({
+            title: "Error",
+            description: res.serverError || "Oops! Something went wrong!",
+            variant: "destructive"
+          });
+      }
       
-        //   if (!response.ok) {
-        //     throw new Error('Network response was not ok');
-        //   }
-      
-        //   const result = await response.json();
-      
-        //   toast({
-        //     title: "Success",
-        //     description: "Certificate has been created successfully!",
-        //     variant: "success"
-        //   });
-        // } catch (error) {
-        //   console.error('Error submitting form:', error); // Log detailed error message
-        //   toast({
-        //     title: "Error",
-        //     description: error.message || "Oops! Something went wrong!",
-        //     variant: "destructive"
-        //   });
-        // }
-      };
+      if (res?.success){
+        toast({
+          title: "Success",
+          description: "Certificate has been created successfully!",
+          variant: "default"
+        });
+      }
+    })
+  };
     
       
-
-  return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)}>
-        <div className="space-y-4">
-          <FormField
-            control={form.control}
-            name="precinct"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Precinct</FormLabel>
-                <FormControl>
-                  <Input placeholder="Precinct#" {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="firstname"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Firstname</FormLabel>
-                <FormControl>
-                  <Input placeholder="Firstname" {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="middlename"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Middlename</FormLabel>
-                <FormControl>
-                  <Input placeholder="Middlename" {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="lastname"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Lastname</FormLabel>
-                <FormControl>
-                  <Input placeholder="Lastname" {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="email"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Email</FormLabel>
-                <FormControl>
-                  <Input placeholder="Enter your email" type="email" {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="birthdate"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Date of Birth</FormLabel>
-                <FormControl>
-                  <div>
-                    <DatePicker 
-                      selected={field.value ? new Date(field.value) : null}
-                      onChange={(date) => field.onChange(date ? date.toISOString().split('T')[0] : '')}
-                    />
-                  </div>
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="contact"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Contact</FormLabel>
-                <FormControl>
-                  <Input placeholder="Contact number" {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-        </div>
-        <Button className="w-full mt-6" type="submit">Submit</Button>
-      </form>
-    </Form>
-  )
+  return StepperDemo()
+  // return (
+  //   <Form {...form}>
+  //     <form onSubmit={form.handleSubmit(onSubmit)}>
+  //       <div className="space-y-4">
+  //         <FormField
+  //           control={form.control}
+  //           name="precinct"
+  //           render={({ field }) => (
+  //             <FormItem>
+  //               <FormLabel>Precinct</FormLabel>
+  //               <FormControl>
+  //                 <Input placeholder="Precinct#" {...field} />
+  //               </FormControl>
+  //               <FormMessage />
+  //             </FormItem>
+  //           )}
+  //         />
+  //         <FormField
+  //           control={form.control}
+  //           name="firstname"
+  //           render={({ field }) => (
+  //             <FormItem>
+  //               <FormLabel>Firstname</FormLabel>
+  //               <FormControl>
+  //                 <Input placeholder="Firstname" {...field} />
+  //               </FormControl>
+  //               <FormMessage />
+  //             </FormItem>
+  //           )}
+  //         />
+  //         <FormField
+  //           control={form.control}
+  //           name="middlename"
+  //           render={({ field }) => (
+  //             <FormItem>
+  //               <FormLabel>Middlename</FormLabel>
+  //               <FormControl>
+  //                 <Input placeholder="Middlename" {...field} />
+  //               </FormControl>
+  //               <FormMessage />
+  //             </FormItem>
+  //           )}
+  //         />
+  //         <FormField
+  //           control={form.control}
+  //           name="lastname"
+  //           render={({ field }) => (
+  //             <FormItem>
+  //               <FormLabel>Lastname</FormLabel>
+  //               <FormControl>
+  //                 <Input placeholder="Lastname" {...field} />
+  //               </FormControl>
+  //               <FormMessage />
+  //             </FormItem>
+  //           )}
+  //         />
+  //         <FormField
+  //           control={form.control}
+  //           name="email"
+  //           render={({ field }) => (
+  //             <FormItem>
+  //               <FormLabel>Email</FormLabel>
+  //               <FormControl>
+  //                 <Input placeholder="Enter your email" type="email" {...field} />
+  //               </FormControl>
+  //               <FormMessage />
+  //             </FormItem>
+  //           )}
+  //         />
+  //         <FormField
+  //           control={form.control}
+  //           name="birthdate"
+  //           render={({ field }) => (
+  //             <FormItem>
+  //               <FormLabel>Date of Birth</FormLabel>
+  //               <FormControl>
+  //                 <div>
+  //                   <DatePicker 
+  //                     selected={field.value ? new Date(field.value) : null}
+  //                     onChange={(date) => field.onChange(date ? date.toISOString().split('T')[0] : '')}
+  //                   />
+  //                 </div>
+  //               </FormControl>
+  //               <FormMessage />
+  //             </FormItem>
+  //           )}
+  //         />
+  //         <FormField
+  //           control={form.control}
+  //           name="contact"
+  //           render={({ field }) => (
+  //             <FormItem>
+  //               <FormLabel>Contact</FormLabel>
+  //               <FormControl>
+  //                 <Input placeholder="Contact number" {...field} />
+  //               </FormControl>
+  //               <FormMessage />
+  //             </FormItem>
+  //           )}
+  //         />
+  //       </div>
+  //       <Button className="w-full mt-6" type="submit">Submit</Button>
+  //     </form>
+  //   </Form>
+  // )
 }
 
 export default CertificateForm;
