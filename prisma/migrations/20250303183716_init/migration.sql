ffcd3f7f77af5f1c25012aff97edaa1c05d27515
aaron <aaron_palpallatoc@dlsu.edu.ph>
2025-03-04 02:42:12 +0800

    Fix db table relations and restart with fresh migrations

--- prisma/migrations/20250303183716_init/migration.sql
+++ prisma/migrations/20250303183716_init/migration.sql
@@ -1,7 +1,3 @@
--- CreateSequence
-CREATE SEQUENCE bahay_toro_system_id_seq START WITH 10001 INCREMENT BY 1;
-CREATE SEQUENCE reference_number_seq START WITH 1 INCREMENT BY 1;
-
 -- CreateEnum
 CREATE TYPE "UserRole" AS ENUM ('ADMIN', 'USER');
 
@@ -23,6 +19,9 @@ CREATE TYPE "ResidencyType" AS ENUM ('HOME_OWNER', 'TENANT', 'HELPER', 'CONSTRUC
 -- CreateEnum
 CREATE TYPE "CertificateType" AS ENUM ('BARANGAY_CLEARANCE', 'BARANGAY_ID', 'SOLO_PARENT', 'COHABITATION', 'GOOD_MORAL', 'NO_INCOME', 'FIRST_TIME_JOB_SEEKER', 'RESIDENCY', 'TRANSFER_OF_RESIDENCY', 'LIVING_STILL', 'BIRTH_FACT');
 
+-- CreateEnum
+CREATE TYPE "CertificateStatus" AS ENUM ('PENDING', 'PROCESSING', 'COMPLETED', 'REJECTED');
+
 -- CreateTable
 CREATE TABLE "User" (
     "id" SERIAL NOT NULL,
@@ -39,7 +38,7 @@ CREATE TABLE "User" (
 -- CreateTable
 CREATE TABLE "Resident" (
     "id" SERIAL NOT NULL,
-    "bahayToroSystemId" TEXT NOT NULL DEFAULT concat('BAHAY-TORO-', to_char(nextval('bahay_toro_system_id_seq'), 'FM00000')),
+    "bahayToroSystemId" TEXT NOT NULL DEFAULT concat('BAHAY-TORO-', to_char(nextval('bahay_toro_system_id_seq'::regclass), 'FM00000'::text)),
     "precinctNumber" TEXT,
     "firstName" TEXT NOT NULL,
     "middleName" TEXT,
@@ -89,12 +88,14 @@ CREATE TABLE "Address" (
 -- CreateTable
 CREATE TABLE "CertificateRequest" (
     "id" SERIAL NOT NULL,
-    "referenceNumber" TEXT NOT NULL DEFAULT concat('VVFJ-', to_char(nextval('reference_number_seq'), 'FM00000')),
+    "referenceNumber" TEXT NOT NULL DEFAULT concat('VVFJ-', to_char(nextval('reference_number_seq'::regclass), 'FM00000'::text)),
     "residentId" INTEGER NOT NULL,
+    "userId" INTEGER NOT NULL,
     "certificateType" "CertificateType" NOT NULL,
     "purpose" TEXT NOT NULL,
     "additionalInfo" JSONB,
     "requestDate" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "status" "CertificateStatus" NOT NULL DEFAULT 'PENDING',
 
     CONSTRAINT "CertificateRequest_pkey" PRIMARY KEY ("id")
 );
@@ -112,36 +113,6 @@ CREATE TABLE "ProofOfIdentity" (
     CONSTRAINT "ProofOfIdentity_pkey" PRIMARY KEY ("id")
 );
 
--- CreateTable
-CREATE TABLE "Certificate" (
-    "id" SERIAL NOT NULL,
-    "precinct" TEXT NOT NULL,
-    "firstname" TEXT NOT NULL,
-    "middlename" TEXT NOT NULL,
-    "lastname" TEXT NOT NULL,
-    "email" TEXT NOT NULL,
-    "birthdate" TIMESTAMP(3) NOT NULL,
-    "contact" TEXT NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL,
-
-    CONSTRAINT "Certificate_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
-CREATE TABLE "BahayToroSystemIdSequence" (
-    "id" SERIAL NOT NULL,
-
-    CONSTRAINT "BahayToroSystemIdSequence_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
-CREATE TABLE "ReferenceNumberSequence" (
-    "id" SERIAL NOT NULL,
-
-    CONSTRAINT "ReferenceNumberSequence_pkey" PRIMARY KEY ("id")
-);
-
 -- CreateIndex
 CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
 
@@ -152,7 +123,7 @@ CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
 CREATE UNIQUE INDEX "Resident_bahayToroSystemId_key" ON "Resident"("bahayToroSystemId");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "Resident_email_key" ON "Resident"("email");
+CREATE UNIQUE INDEX "Resident_id_email_key" ON "Resident"("id", "email");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "EmergencyContact_residentId_key" ON "EmergencyContact"("residentId");
@@ -166,17 +137,17 @@ CREATE UNIQUE INDEX "CertificateRequest_referenceNumber_key" ON "CertificateRequ
 -- CreateIndex
 CREATE UNIQUE INDEX "ProofOfIdentity_residentId_key" ON "ProofOfIdentity"("residentId");
 
--- CreateIndex
-CREATE UNIQUE INDEX "Certificate_email_key" ON "Certificate"("email");
+-- AddForeignKey
+ALTER TABLE "EmergencyContact" ADD CONSTRAINT "EmergencyContact_residentId_fkey" FOREIGN KEY ("residentId") REFERENCES "Resident"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "EmergencyContact" ADD CONSTRAINT "EmergencyContact_residentId_fkey" FOREIGN KEY ("residentId") REFERENCES "Resident"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Address" ADD CONSTRAINT "Address_residentId_fkey" FOREIGN KEY ("residentId") REFERENCES "Resident"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Address" ADD CONSTRAINT "Address_residentId_fkey" FOREIGN KEY ("residentId") REFERENCES "Resident"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "CertificateRequest" ADD CONSTRAINT "CertificateRequest_residentId_fkey" FOREIGN KEY ("residentId") REFERENCES "Resident"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "CertificateRequest" ADD CONSTRAINT "CertificateRequest_residentId_fkey" FOREIGN KEY ("residentId") REFERENCES "Resident"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "CertificateRequest" ADD CONSTRAINT "CertificateRequest_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "ProofOfIdentity" ADD CONSTRAINT "ProofOfIdentity_residentId_fkey" FOREIGN KEY ("residentId") REFERENCES "Resident"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "ProofOfIdentity" ADD CONSTRAINT "ProofOfIdentity_residentId_fkey" FOREIGN KEY ("residentId") REFERENCES "Resident"("id") ON DELETE CASCADE ON UPDATE CASCADE;
