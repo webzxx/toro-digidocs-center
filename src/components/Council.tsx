80d6c2512bd335b555d5ff824e6f24f2f2623d53
aaron <aaron_palpallatoc@dlsu.edu.ph>
2025-03-03 17:05:26 +0800

    Refactor sign in form in Council section to display greeting if there's a user

--- src/components/Council.tsx
+++ src/components/Council.tsx
@@ -1,11 +1,14 @@
 import SignInForm from "@/components/form/SignInForm";
 import Link from "next/link";
+import getSession from "@/lib/getSession";
 
 // React Icons
 import { FaUserCircle } from "react-icons/fa";
 import { FaRegUserCircle } from "react-icons/fa";
 
-const Council = () => {
+const Council = async () => {
+  const session = await getSession();
+
   return (
     <div id="council-section" className="relative w-full py-8">
       <div className="container mx-auto px-4 lg:px-8 min-h-screen flex flex-col">
@@ -24,13 +27,26 @@ const Council = () => {
           {/* Left Column (Login & Forms) */}
           <div className="w-full lg:w-1/2 flex flex-col lg:flex-row space-y-6 lg:space-y-0 lg:space-x-6">
             {/* Members Login */}
-            <div className="bg-white rounded-lg shadow-lg border border-stone-300 p-8 w-full lg:w-1/2">
-              <FaUserCircle className="text-4xl mb-6 mx-auto" size={80} />
-              <h1 className="text-xl font-semibold mb-6 text-center">
-                Members Login
-              </h1>
-              <SignInForm />
-            </div>
+            { !session?.user ? (
+              <div className="bg-white rounded-lg shadow-lg border border-stone-300 p-8 w-full lg:w-1/2">
+                <FaUserCircle className="text-4xl mb-6 mx-auto" size={80} />
+                <h1 className="text-xl font-semibold mb-6 text-center">
+                  Members Login
+                </h1>
+                <SignInForm />
+              </div>
+            ) : (
+              <div className="flex flex-col items-center bg-white rounded-lg shadow-lg border border-stone-300 p-8 w-full lg:w-1/2">
+                <h1 className="text-xl font-semibold mb-6 text-center">
+                  Welcome, {session.user.name} ({session.user.email})
+                </h1>
+                <Link href="/dashboard">
+                  <button className="bg-green-primary text-white px-4 py-2 rounded-md hover:bg-green-600 focus:outline-none focus:ring focus:border-blue-300">
+                    Go to {session.user.role === "ADMIN" ? "Dashboard" : "My Account"}
+                  </button>
+                </Link>
+              </div>
+            )}
 
             {/* Certificate Forms */}
             <div className="bg-white rounded-lg shadow-lg border border-stone-300 p-8 w-full lg:w-1/2">
