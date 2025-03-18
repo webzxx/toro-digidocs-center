fffb89f2831bb034aebd808af0e3cb06e4d52c4d
aaron <aaron_palpallatoc@dlsu.edu.ph>
2025-03-18 18:12:47 +0800

    Fix layout handling element overflow

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -15,7 +15,7 @@ export default async function DashboardLayout({ children, admin, user }: Dashboa
   return (
     <div className="container flex flex-col min-h-screen w-full lg:grid lg:grid-cols-[280px_1fr] mt-6">
       <DashboardSideBar role={role} />
-      <main className="flex flex-col gap-4 pt-4 lg:p-4 lg:gap-6">
+      <main className="w-full overflow-x-hidden flex flex-col gap-4 pt-4 lg:p-4 lg:gap-6">
         {role === "ADMIN" ? admin : user}
       </main>
     </div>
