cd1d6121cc8e2fcb314f8850ead448c1e7850fc8
aaron <aaron_palpallatoc@dlsu.edu.ph>
2025-03-23 07:17:47 +0800

    Remove User unused component

--- src/components/User.tsx
+++
@@ -1,9 +0,0 @@
-import getSession from "@/lib/getSession"
-
-export default async function User() {
-    const session = await getSession();
-
-  return (
-    <pre>{JSON.stringify(session)}</pre>
-  )
-}
