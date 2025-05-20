d0f086a597f021c0c4b1c523cbfbb35a7afb4347
Webster Navarro <nwebzx@gmail.com>
2025-05-20 14:50:36 +0800

    Removed: Create sequences needed for ID generation

--- reset-db.sql
+++ reset-db.sql
@@ -3,8 +3,6 @@ DROP SCHEMA public CASCADE;
 CREATE SCHEMA public;
 
 -- Create sequences needed for ID generation
-CREATE SEQUENCE bahay_toro_system_id_seq;
-CREATE SEQUENCE reference_number_seq;
 
 -- Grant permissions (typically needed for PostgreSQL)
 GRANT ALL ON SCHEMA public TO postgres;
