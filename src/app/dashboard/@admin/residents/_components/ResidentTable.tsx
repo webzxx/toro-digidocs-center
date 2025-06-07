ff36ac8e0bece6b10db9fccbc571970ceb11ae20
Webster Navarro <nwebzx@gmail.com>
2025-06-07 19:32:58 +0800

    Changed hovering view of proof of identity to modal.

--- src/app/dashboard/@admin/residents/_components/ResidentTable.tsx
+++ src/app/dashboard/@admin/residents/_components/ResidentTable.tsx
@@ -18,6 +18,8 @@ import Image from "next/image";
 import ResidentActions from "./ResidentActions";
 import { getGenderBadge, getCivilStatusBadge, getSectorBadge } from "@/components/utils";
 import { ResidentWithRelations } from "@/types/shared";
+import { Dialog, DialogClose, DialogContent, DialogFooter, DialogHeader, DialogTrigger } from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
 
 interface ResidentTableProps {
   residents?: ResidentWithRelations[];
@@ -100,42 +102,53 @@ export default function ResidentTable({ residents, isLoading = false, refetch }:
     );
   };
   
-  const renderProofOfIdentity = (id: number, proof: any) => {
-    if (!proof) return "N/A";
-    
+  const renderProofOfIdentity = (resident: ResidentWithRelations) => {
+    if (!resident.proofOfIdentity) return "N/A";
+    const proof = resident.proofOfIdentity;
+
     return (
-      <TooltipProvider>
-        <Tooltip>
-          <TooltipTrigger className="text-left">View Proof of Identity</TooltipTrigger>
-          <TooltipContent className="w-96 p-0">
-            <div className="rounded-md bg-white p-4 shadow-lg">
-              <h4 className="mb-2 text-lg font-semibold">Proof of Identity</h4>
-              <div className="grid grid-cols-2 gap-2">
-                <div>
-                  <p className="mb-1 text-sm font-medium text-gray-500">ID Photo 1</p>
-                  <Image layout='responsive' width={200} height={150} src={proof.idPhoto1Path} alt="ID Photo 1" className="rounded" />
-                </div>
-                <div>
-                  <p className="mb-1 text-sm font-medium text-gray-500">ID Photo 2</p>
-                  <Image layout='responsive' width={200} height={150} src={proof.idPhoto2Path} alt="ID Photo 2" className="rounded" />
-                </div>
-                <div>
-                  <p className="mb-1 text-sm font-medium text-gray-500">Holding ID Photo 1</p>
-                  <Image layout='responsive' width={200} height={150} src={proof.holdingIdPhoto1Path} alt="Holding ID Photo 1" className="rounded" />
-                </div>
-                <div>
-                  <p className="mb-1 text-sm font-medium text-gray-500">Holding ID Photo 2</p>
-                  <Image layout='responsive' width={200} height={150} src={proof.holdingIdPhoto2Path} alt="Holding ID Photo 2" className="rounded" />
-                </div>
+      <Dialog>
+        <DialogTrigger asChild>
+          <Button variant="outline" size="icon">
+            View
+          </Button>
+        </DialogTrigger>
+        <DialogContent className="sm:max-w-[800px]">
+          <DialogHeader>
+            {resident.bahayToroSystemId} - {`${resident.firstName} ${resident.lastName}`}
+          </DialogHeader>
+          <div className="max-h-[80vh] overflow-y-auto">
+            <h4 className="mb-2 text-lg font-semibold">Proof of Identity</h4>
+            <div className="grid grid-cols-2 gap-2">
+              <div>
+                <p className="mb-1 text-sm font-medium text-gray-500">ID Photo 1</p>
+                <Image layout='responsive' width={200} height={150} src={proof.idPhoto1Path} alt="ID Photo 1" className="rounded" />
               </div>
-              <div className="mt-2">
-                <p className="mb-1 text-sm font-medium text-gray-500">Signature</p>
-                <Image layout='responsive' width={200} height={150} src={proof.signaturePath} alt="Signature" className="rounded" />
+              <div>
+                <p className="mb-1 text-sm font-medium text-gray-500">ID Photo 2</p>
+                <Image layout='responsive' width={200} height={150} src={proof.idPhoto2Path} alt="ID Photo 2" className="rounded" />
+              </div>
+              <div>
+                <p className="mb-1 text-sm font-medium text-gray-500">Holding ID Photo 1</p>
+                <Image layout='responsive' width={200} height={150} src={proof.holdingIdPhoto1Path} alt="Holding ID Photo 1" className="rounded" />
+              </div>
+              <div>
+                <p className="mb-1 text-sm font-medium text-gray-500">Holding ID Photo 2</p>
+                <Image layout='responsive' width={200} height={150} src={proof.holdingIdPhoto2Path} alt="Holding ID Photo 2" className="rounded" />
               </div>
             </div>
-          </TooltipContent>
-        </Tooltip>
-      </TooltipProvider>
+            <div className="mt-2">
+              <p className="mb-1 text-sm font-medium text-gray-500">Signature</p>
+              <Image layout='responsive' width={200} height={150} src={proof.signaturePath} alt="Signature" className="rounded" />
+            </div>
+          </div>
+          <DialogFooter>
+            <DialogClose asChild>
+              <Button>Close</Button>
+            </DialogClose>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
     );
   };
   
@@ -190,7 +203,7 @@ export default function ResidentTable({ residents, isLoading = false, refetch }:
                 <TableCell>{resident.sector ? getSectorBadge(resident.sector) : "N/A"}</TableCell>
                 <TableCell>{renderAddress(resident.address)}</TableCell>
                 <TableCell>{renderEmergencyContact(resident.emergencyContact)}</TableCell>
-                <TableCell>{renderProofOfIdentity(resident.id, resident.proofOfIdentity)}</TableCell>
+                <TableCell>{renderProofOfIdentity(resident)}</TableCell>
                 <TableCell>
                   <ResidentActions resident={resident} refetch={refetch}/>
                 </TableCell>
